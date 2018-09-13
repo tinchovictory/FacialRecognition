23fe7e6ba5f64e3e6a8dd236b47b948df3d9c527
tinchovictory <martin@victory.com.ar>
2018-09-13 18:05:36 -0300

    Face recognition working using our own eigenvectors calculation

--- face.py
+++ face.py
@@ -11,12 +11,10 @@ IMG_HEIGHT = 112
 IMG_WIDTH = 92
 
 
-'''
-   Load images from the folder path, and returns them as a matrix with the full image as a row.
-   Each image loaded is a row of the matrix.
-   The path directory should have the images grouped in subfolders, where each subfolder is a diferent person.
-   Each person should have IMAGES_PER_PERSON images. 
-'''
+#   Load images from the folder path, and returns them as a matrix with the full image as a row.
+#   Each image loaded is a row of the matrix.
+#   The path directory should have the images grouped in subfolders, where each subfolder is a diferent person.
+#   Each person should have IMAGES_PER_PERSON images. 
 def imageLoader(path):
     directories = [d for d in listdir(path) if isdir(join(path, d))]
 
@@ -35,14 +33,15 @@ def imageLoader(path):
 
 
 
-'''
-   Get Q, R from matrixA.
-   Implemented using the householder method, which is computationaly better than doing gram-schmidt.
-   It also gets Q and R at the same time.
-   This algorithm is O( 2*m*n^2 - (2/3)*n^3 ), while gram-schmidt is O( 2*m*n^2 )
-   The householder algorithm is the most used algorithm for Q, R.
-   MATLAB implement householder
-'''
+#   Get Q, R from matrixA.
+#   Implemented using the householder method, which is computationaly better than doing gram-schmidt.
+#   It also gets Q and R at the same time.
+#   This algorithm is O( 2*m*n^2 - (2/3)*n^3 ), while gram-schmidt is O( 2*m*n^2 )
+#   The householder algorithm is the most used algorithm for Q, R.
+#   MATLAB implement householder
+#   Code inspired in:
+#       http://www.seas.ucla.edu/~vandenbe/133A/lectures/qr.pdf
+#       https://www.cs.cornell.edu/~bindel/class/cs6210-f09/lec18.pdf
 def getQandR(matrixA):
     m, n = matrixA.shape
     Q = np.eye(m)
@@ -60,73 +59,106 @@ def getQandR(matrixA):
 
         R[j:,:] -= np.matmul((tau * w), wTxR)
         Q[:,j:] -= np.dot( np.dot(Q[:, j:], w) , np.transpose(tau * w) )
-
     return Q, R 
 
 
 
-'''
-   Get the eigenvalues and eigenvectors of matrixA. 
-   Returns eigenvalues as an array, 
-   and eigenvectors as a matrix where the i column is the eigenvector of the i eigenvalue.
-   The algorithm uses Q, R factorization, and the Q, R algorithm.
-   To get the eigenvector matrix (S), just uses the algorithm:
-       S = Q0
-       Sn+1 = Sn * Qn 
-'''
+#   Get the eigenvalues and eigenvectors of matrixA. 
+#   Returns eigenvalues as an array, 
+#   and eigenvectors as a matrix where the i column is the eigenvector of the i eigenvalue.
+#   The algorithm uses Q, R factorization, and the Q, R algorithm.
+#   To get the eigenvector matrix (S), just uses the algorithm:
+#       S = Q0
+#       Sn+1 = Sn * Qn 
+#   This algorithm iterates 100 times the Q, R algorithm, or stops when the result
+#   matrix A is diagonal. It's assumed that the matrix is diagonal when the values 
+#   outside the diagonal are less than 0.0001
+#   Code inspired in:
+#       https://www.physicsforums.com/threads/how-do-i-numerically-find-eigenvectors-for-given-eigenvalues.561763/
+#       http://www-users.math.umn.edu/~olver/aims_/qr.pdf
 def getEigenvaluesAndEigenvectors(matrixA):
     A = np.copy(matrixA).astype(float)
     S = np.eye(matrixA.shape[0])
 
-    for i in range(0,1500): #choose correct range, 1500 is just for testing
+    for i in range(0, 100): # 100 is the default QR iteration limit
         Q, R = getQandR(A)
         A = np.matmul(R, Q)
         S = np.matmul(S, Q)
 
+        aux = A - np.diag(np.diag(A))
+        if np.absolute(aux).max() < 0.0001:
+            break
+
     return np.diag(A), S
 
 
+#   According to SVD, a matrix A = U * S * V'
+#   This function returns V'
+#   The columns of V are the eigenvectors of A'A, this matrix 
+#   is to big to get it's eigenvectors, so they are calculated using U
+#   The columns of U are the eignevectors of AA'
+#   v = A' * u / singVal, where v are the columns of V, u are the columns of U,
+#   singVal are the singular values from the diagonal matrix S
+#   The singular values are related to the eigenvalues as: singVal = sqrt(eigenvalue)
+#   This algorithm stop getting the columns of V when the realted eigenvalues is less
+#   than 0.00001
+def getVofSVD(matrixA):
+    At = np.transpose(matrixA)
+    AAt = np.matmul(matrixA, At)
+
+    eigenvalues, U = getEigenvaluesAndEigenvectors(AAt) # U are the eigenvectors
+
+    n = matrixA.shape[1]
+    V = np.zeros([n,n])
+
+    for i in range(0, U.shape[1]):
+
+        # Stop when eigenvalue is too small
+        if eigenvalues[i] < 0.00001:
+            break
+
+        u = np.reshape(U[:, i], (-1, 1))
+        v = np.matmul(At, u) / np.sqrt(eigenvalues[i])
+        
+        V[i,:] = np.transpose(v)
+
+    return np.transpose(V)
+    
 
 
 
+### Code for facial recognition ###
 
-# Code of facial recognition working, using SVD
-'''
-images, persons = imageLoader("facesvddem/")
+print("Loading images")
 
+images, persons = imageLoader("att_faces/")
 
 meanImage = np.mean(images, 0)
 images -= meanImage
 
-U, S, V = np.linalg.svd(images, full_matrices=False)
+print("Generating autofaces")
 
+#U, S, V = np.linalg.svd(images, full_matrices=False)
+V = getVofSVD(images)
 
 projected = np.dot(images, np.transpose(V))
 
+# Using support vector machine to predict the test image
+
+print("Loading SVM")
 
 clf = svm.LinearSVC()
 clf.fit(projected, persons)
 
-testImg = im.imread("facesvddem/s3/10.pgm")/MAX_BYTE_VALUE
+print("Loading test image")
+
+testImg = im.imread("att_faces/s12/10.pgm") / MAX_BYTE_VALUE
 testImg = np.reshape(testImg, [1, IMG_HEIGHT * IMG_WIDTH])
 
 testImg -= meanImage
 
 projectedTestImg = np.dot(testImg, np.transpose(V))
 
-print(clf.predict(projectedTestImg))
+print("Predicting image")
 
-'''
-
-
-
-# Debuging Eigenvalues and Eigenvectors
-
-A = np.array([[2, 1, 2], [2, 2, 1], [1, 2, 2]])
-#Q, R = getQandR(A)
-eigenvalues, eigenvectors = getEigenvaluesAndEigenvectors(A)
-
-
-print(A)
-print(eigenvalues)
-print(eigenvectors)
+print("The test image is: " + str(clf.predict(projectedTestImg)[0]))
